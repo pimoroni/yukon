0bdca56a46885b1533993c4f413bc24a92e0d05b
ZodiusInfuser <christopher.parrott2@gmail.com>
2023-08-22 11:34:20 +0100

    Added main example

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-# This file (and other files in this directory) get added to a LittleFS filesystem and appended to the firmware
+# This file runs at power-up. It and files in the /lib directory get added to a LittleFS filesystem and appended to the firmware
+# Overide this file with any of your own code that you wish to run at power-up, or remove the file to not have anything run.
+
+import time
+from pimoroni_yukon import Yukon
+
+"""
+A program to show that Yukon is active, by flashing the onboard LEDs in sequence.
+"""
+
+
+# Create a Yukon object to begin using the board
+yukon = Yukon()
+
+# Loop forever, or until a stop command from Thonny or another IDE is received
+while True:
+    yukon.set_led('A', True)
+    time.sleep(0.2)
+    yukon.set_led('B', True)
+    time.sleep(0.2)
+    yukon.set_led('A', False)
+    time.sleep(0.2)
+    yukon.set_led('B', False)
+    time.sleep(1.4)
